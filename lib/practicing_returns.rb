e91958dc41e0c1af25fafe8822b30b93fc78ec30
Sharnell Grant <sharnellgrant@gmail.com>
2020-02-17 04:46:49 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -3,10 +3,11 @@ require 'pry'
 def hello(array)
   i = 0
   while i < array.length #use while loop to iterate
+  binding.pry
     yield(array[i]) #yielding each member of the array into the block
     i += 1
   end
 end
 
-binding.pry
+
 hello(["Tim", "Tom", "Jim"]) { |name|  "Hi, #{name}" }
