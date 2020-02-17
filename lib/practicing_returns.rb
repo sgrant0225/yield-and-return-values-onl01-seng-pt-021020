d1eadbdd0e0ad21619f96b4bef462e28bf2c4975
Sharnell Grant <sharnellgrant@gmail.com>
2020-02-17 04:41:03 +0000

    Automatically backed up by Learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -2,8 +2,8 @@ require 'pry'
 
 def hello(array)
   i = 0
-  while i < array.length
-    yield(array[i])
+  while i < array.length #use while loop to iterate
+    yield(array[i]) #yielding each member of the array into the block
     i += 1
   end
 end
